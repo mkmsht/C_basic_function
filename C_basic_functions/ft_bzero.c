6d09be780dd9177f949ede863204df6c01a40e76
Shota Mikami <shota79.s20@gmail.com>
2020-08-16 05:32:22 +0900

    change directory name.

--- C_basic_functions/ft_bzero.c
+++
@@ -1,6 +0,0 @@
-#include "libft.h"
-
-void	ft_bzero(void *buf, size_t len)
-{
-	ft_memset(buf, 0, (size_t)len);
-}
--- C_basic_functions/ft_putstr_fd.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-void	ft_putstr_fd(char *s, int fd)
-{
-	int s_len;
-
-	s_len = ft_strlen(s);
-	write(fd, s, s_len);
-}
